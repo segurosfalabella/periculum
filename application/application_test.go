f67962f9aa4c0cca3212b864ed4b0015060bc204
Sandro Ariel Gómez Araya <sandro.gomez@gmail.com>
2018-07-01 21:30:31 -0400

    fix(yml): Remove useless files to test

--- application/application_test.go
+++ application/application_test.go
@@ -1,13 +1,24 @@
 package application_test
 
 import (
+	"io/ioutil"
 	"reflect"
 	"testing"
 
 	"github.com/mrsangrin/periculum/application"
 )
 
+var tempFilePATH = "../tmp/apps.yml"
+
 func TestRemoteServices_GetApps(t *testing.T) {
+	var yamlFile = `version: 3.2	
+services:
+  - name: dummy
+    description: dummy
+    endpoint: dummy`
+
+	ioutil.WriteFile(tempFilePATH, []byte(yamlFile), 0644)
+
 	tests := []struct {
 		name    string
 		c       application.RemoteServices
@@ -26,16 +37,10 @@ func TestRemoteServices_GetApps(t *testing.T) {
 			want:    nil,
 			wantErr: true,
 		},
-		{
-			name:    "ShouldBeFailIfYAMLHasNotService",
-			c:       application.RemoteServices{},
-			want:    nil,
-			wantErr: true,
-		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.c.GetApps("../apps-test.yml").Services; !reflect.DeepEqual(got, tt.want) != tt.wantErr {
+			if got := tt.c.GetApps(tempFilePATH).Services; !reflect.DeepEqual(got, tt.want) != tt.wantErr {
 				t.Errorf("RemoteServices.GetApps() = %v, want %v", got, tt.want)
 			}
 
